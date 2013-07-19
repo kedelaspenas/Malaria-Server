421d717139faff9e436f2b9135914f777e7b00aa
Noel Sison II <noelnicanoronacin@yahoo.ca>
2013-07-19 17:33:06 +0800

    some dynamic content
    on records table, case information and images

--- serveus/views.py
+++ serveus/views.py
@@ -13,7 +13,23 @@ def dashboard():
 
 @app.route('/records')
 def records():
-    return render_template("records.html")
+
+    # Dummy records object
+    class Record:
+        date = "06/07/2013"
+        patient_id = 101
+        location = "Bicol Region"
+        diagnosis = "Vivax"
+        '''
+        def __init__(self, date, patient_id, location, diagnosis):
+            self.date = date
+            self.patient_id = patient_id
+            self.location = location
+            self.diagnosis = diagnosis
+        '''
+    list = [Record(), Record(), Record(), Record(), Record()]
+        
+    return render_template("records.html", list = list)
 
 @app.route('/map')
 def map():
@@ -21,7 +37,17 @@ def map():
 
 @app.route('/case')
 def case():
-    return render_template("case.html")
+    class Case:
+        patient_id = 101
+        date = "06/07/2013"
+        name = "Raigor Stonehoof"
+        age = 49
+        address = "41 Real Street, Some Subdivision, Bicol Region"
+        human_diagnosis = "Vivax"
+        computer_diagnosis = "Falciparum, Vivax"
+        images = ["1.png", "2.png", "3.png", "4.png"]
+
+    return render_template("case.html", case = Case())
 
 @app.route('/logout')
 def logout():
