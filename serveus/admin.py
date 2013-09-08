bf017fff0f456c72ec101f2ef55fba5123f19504
Noel Sison II <noelnicanoronacin@yahoo.ca>
2013-09-08 16:08:55 +0800

    autherntication on admin, masked password field, fixed navbar urls, recovered my account

--- serveus/admin.py
+++ serveus/admin.py
@@ -1,11 +1,43 @@
 from serveus import app
-from flask.ext.admin import Admin
+from flask.ext.admin import Admin, AdminIndexView
 from flask.ext.admin.contrib.sqlamodel import ModelView
+from flask.ext.wtf import PasswordField
+from flask.ext.login import current_user
 
 from models import db, User, UserType, Case, MalType
 
-admin = Admin(app)
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(UserType, db.session))
-admin.add_view(ModelView(Case, db.session))
-admin.add_view(ModelView(MalType, db.session))
+# Custom admin index
+
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated()
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated()
+        
+admin = Admin(app, index_view=MyAdminIndexView())
+
+# Custom admin pages
+# More info in https://github.com/mrjoes/flask-admin/issues/173
+class UserView(MyModelView):
+    can_create = True
+    column_list = ('username', 'usertype')
+    column_excluded_list = ('password')
+    #column_searchable_list = ('username')
+    
+    def scaffold_form(self):
+        form_class = super(UserView, self).scaffold_form()
+        form_class.password = PasswordField()
+        return form_class
+        
+    def on_model_change(self, form, model):
+        if len(model.password):
+            model.password = User.hash_password(form.password.data)
+            
+# Add pages to the admin page
+#admin.add_view(ModelView(User, db.session))
+admin.add_view(MyModelView(UserType, db.session))
+admin.add_view(UserView(User, db.session))
+admin.add_view(MyModelView(Case, db.session))
+admin.add_view(MyModelView(MalType, db.session))
