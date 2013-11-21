856db20d1a34a782cd0f32f5df5d056f5bb1337e
Noel Sison II <noelnicanoronacin@yahoo.ca>
2013-11-21 14:35:53 +0800

    show ip on console, gave up on blueprints

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import sys
-
+import socket
 from serveus import app
 
 port = 5000
 if len(sys.argv) > 1 and sys.argv[1]:
 	port = int(sys.argv[1])
+print ' * IP: ' + socket.gethostbyname(socket.gethostname()) + ':' + str(port)
 app.run('0.0.0.0', port=port, debug = True)
