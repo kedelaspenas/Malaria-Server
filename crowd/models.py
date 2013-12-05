f5b379a4129bc9a845594c5fae185731875b5927
Noel Sison II <noelnicanoronacin@yahoo.ca>
2013-12-05 12:31:12 +0800

    added crowdsourcing models

--- crowd/models.py
+++ crowd/models.py
@@ -17,9 +17,21 @@ db.drop_all()
 Column types: http://pythonhosted.org/Flask-SQLAlchemy/models.html
 '''
 
+class Labeler(db.Model):
+    __tablename__ = 'labeler'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    total_images_labeled = db.Column(db.Integer)
+    total_correct_images_labeled = db.Column(db.Integer)
+    last_session = db.Column(db.DateTime())
+    labeler_rating = db.Column(db.Float)
+    labeler_type_id = db.Column(db.Integer, db.ForeignKey('labelertype.id'))
+
 class LabelerType(db.Model):
+    __tablename__ = 'labelertype'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
+    labelers = db.relationship('Labeler', backref='labelertype', lazy='dynamic')
 
     # Optional
     def __init__(self, name=""):
@@ -30,17 +42,34 @@ class LabelerType(db.Model):
 
     def __str__(self):
         return self.name
-    
-'''
-class Labeler(db.Model):
-    pass
 
 class TrainingImage(db.Model):
-    pass
-
-class ImageLabel(db.Model):
-    pass
+    __tablename__ = 'trainingimage'
+    id = db.Column(db.Integer, primary_key=True)
+    image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
+    total_labels = db.Column(db.Integer)
+    final_label = db.Column(db.Integer, db.ForeignKey('maltype.id'))
+    date_finalized = db.Column(db.DateTime())
+    training_image_labels = db.relationship('TrainingImageLabel', backref='trainingimage', lazy='dynamic')
+    
+class TrainingImageLabel(db.Model):
+    __tablename__ = 'trainingimagelabel'
+    id = db.Column(db.Integer, primary_key=True)
+    training_image_id = db.Column(db.Integer, db.ForeignKey('trainingimage.id'))
+    date = db.Column(db.Date)
+    # is there a db column type na duration?
+    time_start = db.Column(db.Time)
+    time_end = db.Column(db.Time)
+    initial_label = db.Column(db.Integer, db.ForeignKey('maltype.id'))
+    correct_label = db.Column(db.Integer, db.ForeignKey('maltype.id'))
+    labeler_correct = db.Column(db.Boolean)
+    cells = db.relationship('TrainingImageLabelCell', backref='trainingimagelabel', lazy='dynamic')
 
 class TrainingImageLabelCell(db.Model):
-    pass
-'''
+    __tablename__ = 'trainingimagelabelcell'
+    id = db.Column(db.Integer, primary_key=True)
+    training_image_label_id = db.Column(db.Integer, db.ForeignKey('trainingimagelabel.id'))
+    x = db.Column(db.Float)
+    y = db.Column(db.Float)
+    nearest_cell_x = db.Column(db.Float)
+    nearest_cell_y = db.Column(db.Float)
