a6a7e08069b3dd10dd8a0d526ae63a5dd2ba83ec
Noel Sison II <noelnicanoronacin@yahoo.ca>
2014-03-11 13:50:24 +0800

    stdout to root logger
    not sure if it will fix the error if no console

--- serveus/__init__.py
+++ serveus/__init__.py
@@ -33,17 +33,17 @@ except OSError:
     pass
     
 class StreamToLogger(object):
-    def __init__(self, logger, file_handler, log_level=logging.INFO):
+    def __init__(self, logger, log_level=logging.INFO):
       self.logger = logger
       self.log_level = log_level
       self.linebuf = ''
  
     def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
-         
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+        
     def flush(self):
-        pass
+        self.logger.flush()
          
 
 logging.basicConfig(
@@ -55,10 +55,8 @@ logging.basicConfig(
 formatter = logging.Formatter("> %(message)s")
 file_handler = RotatingFileHandler(LOG_FILE, maxBytes=5*1024*1024, backupCount = 5)
 file_handler.setFormatter(formatter) 
-
-sys.stdout  = StreamToLogger(logging.getLogger('STDOUT'), file_handler, logging.INFO)
-
 root = logging.getLogger('')
+sys.stderr = StreamToLogger(root, logging.INFO)
 root.addHandler(file_handler)
 
 # IMPORT PARTS OF APP
