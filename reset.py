f013b396d163cb779a4dd12b46f0bc0ad2046ce4
Juan Felipe Coronel <juanfelipe.coronel@gmail.com>
2014-04-03 09:32:08 +0800

    Add crowd reset

--- reset.py
+++ reset.py
@@ -3,6 +3,7 @@ import datetime
 import sqlite3
 
 from serveus.models import *
+from crowd.models import *
 
 if os.path.isfile('cs198pythontest.db'):
 	os.remove('cs198pythontest.db')
@@ -2018,6 +2019,43 @@ db.session.add(Key(private_key, public_key))
 public_key = """009b32240dca3fcdce3ed04018fc9c0c7758a3f2e2bef63a79f0fc4f4a418d605307a946d08a620273f6dca9bef7e2ef3351385a79a3c3812f99955854a1c4c221e7b7a7b584696541ad4d9b5bed84f9071dd933914f290cc77090f23c9165a095ba574fa39a6652a2cbcce27ad9927556dc300176689569d197b5dcbd0cd7afc9"""
 private_key = """-----BEGIN RSA PRIVATE KEY-----\nMIICXQIBAAKBgQCbMiQNyj/Nzj7QQBj8nAx3WKPy4r72Onnw/E9KQY1gUwepRtCK\nYgJz9typvvfi7zNROFp5o8OBL5mVWFShxMIh57entYRpZUGtTZtb7YT5Bx3ZM5FP\nKQzHcJDyPJFloJW6V0+jmmZSosvM4nrZknVW3DABdmiVadGXtdy9DNevyQIDAQAB\nAoGAF7J9TNnAClXew3+2EQRm5uZTCmhTDlf5fLGaDdWal8W12sQkXaz/gOOF6Clv\nwmgR5un67q3x0U0KX4KAUb8wgS2wlkWYlT062+mE7cYKZh07ZxCFy+yrfM6qRrvA\nHJPa60TTAQTRXSeoX9DrWHld+JVKml74vV3oQSThdgj70d0CQQC1Ei48dhfmCurY\nsVPjHR6Sx0URqtHvZMHJrW1eMa0X0QHSs9iSXFZmQGD5qprQBjNmMrQe8zd4Uwto\ntPMnPSxjAkEA22rd7deGlaWaaTosMF12ikzMd1KP42IK+BWMIh6HfoXcxj7H1pQ7\nS7oSNgqqIojgGLZfIc9bKfa0sSp6GE+c4wJBAKd2vRRmFAxKJJFsz6zJDbGqYpLI\nbYj+osunffMT9oaEYy8/7hjPFYlUGVxPEQc79OWcF0JYpwC9rVuVnxy3UwkCQQCw\nnCk8OyGyLFTIZDGTUHeMxFpDpSn6PT1VCIr+H5KyLW9SBtB1kGTWBFSKOTVOjNvM\nKGcUYMIhWdmBTQ5vqQ0/AkAx2XcV90V/6i89F2SElT3OcQsjznPJ58TupP5Ckr6l\n3i8SY+/pip4zPV0/gdS21VbaOEvwxaVOUcm1anNmBmRs\n-----END RSA PRIVATE KEY-----"""
 db.session.add(Key(private_key, public_key))
+
+# CROWDSOURCING START
+
+#LabelerType
+db.session.add(LabelerType('Novice'))
+db.session.add(LabelerType('Regular'))
+db.session.add(LabelerType('Expert'))
+
+#Labeler
+# comment out the loop so rodolfo stays as a novice
+'''
+for i in xrange(1,7):
+    db.session.add(Labeler(i,0,0,None,datetime.datetime(2013,12,5,0,0), 1.0, (i%3)+1))
+'''
+db.session.add(Labeler(1,0,0,None,datetime.datetime(2013,12,5,0,0), 1.0, 1))
+db.session.add(Labeler(2,0,0,None,datetime.datetime(2013,12,5,0,0), 1.0, 2))
+db.session.add(Labeler(3,0,0,None,datetime.datetime(2013,12,5,0,0), 1.0, 3))
+db.session.add(Labeler(4,0,0,None,datetime.datetime(2013,12,5,0,0), 1.0, 2))
+db.session.add(Labeler(5,0,0,None,datetime.datetime(2013,12,5,0,0), 1.0, 2))
+db.session.add(Labeler(6,0,0,None,datetime.datetime(2013,12,5,0,0), 1.0, 2))
+
+#TrainingImage
+for i in xrange(1,9):
+    db.session.add(TrainingImage(i,0,'Unlabeled','Unlabeled',None))
+'''
+#TrainingImageLabel
+for i in xrange(1,9):
+    for j in range(1,9):
+        db.session.add(TrainingImageLabel((j%3)+1, i,datetime.date(2013,12,5),datetime.time(j,j), datetime.time(j,j+2),1, None, None))
+
+#TrainingImageLabelCell
+for i in xrange(1,9):
+    for j in xrange(1,9):
+        for k in xrange(1,9):
+            db.session.add(TrainingImageLabelCell(j,k*100,k*100, None,None))
+'''
+log('dummy crowdsourcing data')
 
 # COMMIT
 db.session.commit()
